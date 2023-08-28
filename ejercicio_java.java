dabacfda942d9c584ec38fbe0cd69abdb52cda39
CorbaYe <corba.ye99@gmail.com>
2023-08-28 07:21:10 -0500

    Función fntMenu

--- ejercicio_java.java
+++ ejercicio_java.java
@@ -1,7 +1,20 @@
 import javax.swing.JOptionPane;
 
 public class ejercicio_java{
+
+    static String [] notas = new String[10];
+    static int posicion= 0;
+
+    private static void fntMenu(boolean m){
+        while(m == true){
+            int menu = Integer.parseInt(JOptionPane.showInputDialog(null,"<<< Menú principal >>>\n" +
+            "1. Registrar nota\n" +
+            "2. Mostrar registros\n" +
+            "3. Salir\n"));
+        }
+    }
+
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "args, null, 0, null");
+        fntMenu(true);
     }
 }
